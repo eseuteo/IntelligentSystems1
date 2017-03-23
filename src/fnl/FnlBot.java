db05890ba12e8db39ea9fee57ca5ff51f5b4ca1c
Suto <ricardoholthausen@gmail.com>
2017-03-23 09:12:59 +0100

    Robocode project creation

---
+++ src/fnl/FnlBot.java
@@ -0,0 +1,21 @@
+package fnl;
+
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class FnlBot extends Robot {
+	public void run() {
+		turnLeft(getHeading() % 90);
+		turnGunRight(90);
+		while(true) {
+			int randomNum = ThreadLocalRandom.current().nextInt(0, 100);
+			ahead(1000);
+			turnRight(randomNum);
+		}
+	}
+	
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(5);
+	}
+}
