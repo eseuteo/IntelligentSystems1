88549c8302a3234860951097ccf84b6c19511f18
MrYompi <Ojedamorala@hotmail.com>
2017-03-30 11:07:19 +0200

    Update RouteBot.java

--- src/fnl/RouteBot.java
+++ src/fnl/RouteBot.java
@@ -22,15 +22,14 @@ public class RouteBot extends Robot {
 				map[k][j]=true;
 			}
 		}
-		//route=Astar(getX(), getY(), goalX, goalY, map);
-		//la idea es guardar en map qu� baldosas est�n ocupadas seg�n se van generando las posiciones aleatorias
-		//y calcular la ruta antes de empezar a mover el robot
-		Random rand = new Random(getHeading());
+		int x=(int)getX()/64;
+		int y=(int)getY()/64;
+		map[x][y]=false;
+		Random rand = new Random((int)getHeading());
 		for (int j = 1; j < 51; j++) {
-
-			int x = rand.nextInt();
+			x = rand.nextInt();
 			x = Math.abs(x);
-			int y = (x/MAP_SIZE)%MAP_SIZE;
+			y = (x/MAP_SIZE)%MAP_SIZE;
 			x%=MAP_SIZE;
 			boolean done = false;
 			while (!done) {
@@ -111,10 +110,8 @@ public class RouteBot extends Robot {
 			d=direction.NORTH;
 			break;
 		}
-
 		route.set(i%route.size(), d);
 	}
-
 	public double trigonometry(double DestX, double DestY) {
 		double x = DestX - getX();
 		double y = DestY - getY();
@@ -124,9 +121,134 @@ public class RouteBot extends Robot {
 		}
 		return ang;
 	}
+	public void onHitRobot(HitRobotEvent event) {
+		turnRight(event.getBearing());
+		back(48);
+	}
+*/
+}package fnl;
 
+import robocode.HitRobotEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import java.util.ArrayList;
+import java.util.Random;
 
+public class RouteBot extends Robot {
+	protected enum direction{
+		NORTH, EAST, SOUTH, WEST;
+	}
+	private ArrayList<direction> route = new ArrayList<direction>();
+	static int MAP_SIZE = 13;
+	int i = 0;
+	boolean scanned=false;
 
+	public void run() {
+		boolean[][] map=new boolean[MAP_SIZE][MAP_SIZE];
+		for(int k=0;k<MAP_SIZE;k++){
+			for(int j=0;j<MAP_SIZE;j++){
+				map[k][j]=true;
+			}
+		}
+		int x=(int)getX()/64;
+		int y=(int)getY()/64;
+		map[x][y]=false;
+		Random rand = new Random((int)getHeading());
+		for (int j = 1; j < 51; j++) {
+			x = rand.nextInt();
+			x = Math.abs(x);
+			y = (x/MAP_SIZE)%MAP_SIZE;
+			x%=MAP_SIZE;
+			boolean done = false;
+			while (!done) {
+				if(!map[x][y]){
+					 x = rand.nextInt();
+					 x = Math.abs(x);
+					 y = (x/MAP_SIZE)%MAP_SIZE;
+					 x%=MAP_SIZE;
+				}else{
+					done=true;
+					map[x][y]=false;
+				}
+			}
+		}
+		route.add(direction.NORTH);
+		route.add(direction.EAST);
+		route.add(direction.SOUTH);
+		route.add(direction.WEST);
+		while (true) {
+			do{
+				scanned=false;
+				moveTowards(route.get(i%route.size()));
+				scan();
+			}while(scanned);
+			ahead(64);
+			i++;
+		}
+	}
+
+	public void moveTowards(direction d) {
+		double ang;
+		switch(d){
+			case NORTH:
+				ang=0-getHeading();
+				break;
+			case EAST:
+				ang=90-getHeading();
+				break;
+			case SOUTH:
+				ang=180-getHeading();
+				break;
+			default:
+				ang=-90-getHeading();
+				break;
+		}
+		if (ang > 180.0) {
+			ang = ang - 360.0;
+		} else if (ang < -180.0) {
+			ang = ang + 360.0;
+		}
+		turnRight(ang);
+	}
+
+	protected static int Manhattan(int orX, int orY, int destX, int destY){
+		return Math.abs(destX-orX)+Math.abs(destY-orY);
+	}
+	/*
+	@Override
+	public void onScannedRobot(ScannedRobotEvent event) {
+		if (event.getDistance() <= 64 &&!scanned) {
+			scanned=true;
+			changeDir();
+		}
+	}
+	public void changeDir(){
+		direction d;
+		switch(route.get(i%route.size())){
+		case NORTH:
+			d=direction.EAST;
+			break;
+		case EAST:
+			d=direction.SOUTH;
+			break;
+		case SOUTH:
+			d=direction.WEST;
+			break;
+		default:
+			d=direction.NORTH;
+			break;
+		}
+		route.set(i%route.size(), d);
+	}
+	public double trigonometry(double DestX, double DestY) {
+		double x = DestX - getX();
+		double y = DestY - getY();
+		double ang = Math.toDegrees(Math.atan(x / y));
+		if (y < 0) {
+			ang = ang + 180.0;
+		}
+		return ang;
+	}
 	public void onHitRobot(HitRobotEvent event) {
 		turnRight(event.getBearing());
 		back(48);
